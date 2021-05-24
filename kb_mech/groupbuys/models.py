2ebffec7117bd956ea2b6a0e70ca02275ff75147
Cindey Caine <cindeycaine@Cindeys-MacBook-Pro.local>
2021-05-23 18:09:19 -0700

    Add more fields to group buy

--- kb_mech/groupbuys/models.py
+++ kb_mech/groupbuys/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.utils import timezone
+from datetime import datetime
 
 class GroupBuy(models.Model):
 
@@ -18,8 +20,9 @@ class GroupBuy(models.Model):
         ('CB', 'Cables'),
     ]
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    gb_name = models.CharField(max_length=200)
+    gb_start = models.DateField(default=timezone.now)
+    gb_end = models.DateField(default=timezone.now)
 
     gb_status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     gb_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
